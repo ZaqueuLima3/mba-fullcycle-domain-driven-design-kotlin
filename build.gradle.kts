5394fcee01741afa731464273de29a401c80c1d6
Zaqueu Santos <zaqueulima1@gmail.com>
2024-05-31 14:58:38 -0300

    chore: add hobernate and mysql

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.5"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
+    kotlin("plugin.jpa") version "1.9.24"
 }
 
 group = "dev.zaqueu.domain-driven-design-kotlin"
@@ -19,9 +20,21 @@ repositories {
 }
 
 dependencies {
+    // spring
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // jackson
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // mysql
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // tests
+    testRuntimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
